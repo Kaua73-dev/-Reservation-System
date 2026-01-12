ba3a1f57c291887f86f762d4800a5b9b732dedc0
kaua <kaua.santanaj@gmail.com>
2026-01-12 16:30:11 -0300

    add response in dto for resource

---
+++ src/main/java/com/kaua/reservation/dto/response/ResourceResponse.java
@@ -0,0 +1,6 @@
+package com.kaua.reservation.dto.response;
+
+import com.kaua.reservation.entity.enums.ResourceStatus;
+
+public record ResourceResponse(String name, String category, int capacity, ResourceStatus status) {
+}
