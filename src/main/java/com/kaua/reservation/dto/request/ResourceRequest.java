61b0782ee4d8dae76461a650de5fef97ae43f91c
kaua <kaua.santanaj@gmail.com>
2026-01-12 16:28:49 -0300

    add request in dto for resource

--- src/main/java/com/kaua/reservation/dto/request/ResourceRequest.java
+++ src/main/java/com/kaua/reservation/dto/request/ResourceRequest.java
@@ -1,4 +1,6 @@
 package com.kaua.reservation.dto.request;
 
-public record ResourceRequest() {
+import com.kaua.reservation.entity.enums.ResourceStatus;
+
+public record ResourceRequest(String name, String category, int capacity, ResourceStatus status) {
 }
