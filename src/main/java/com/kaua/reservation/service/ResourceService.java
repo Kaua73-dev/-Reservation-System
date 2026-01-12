21d7985e792b35dd9a36a2c7029d08531882f52a
kaua <kaua.santanaj@gmail.com>
2026-01-12 16:33:38 -0300

    add exception for resource and alter method createResource

--- src/main/java/com/kaua/reservation/service/ResourceService.java
+++ src/main/java/com/kaua/reservation/service/ResourceService.java
@@ -1,13 +1,15 @@
 package com.kaua.reservation.service;
 
 
+import com.kaua.reservation.auth.AuthVerifyService;
 import com.kaua.reservation.dto.request.ResourceRequest;
 import com.kaua.reservation.dto.response.ResourceResponse;
+import com.kaua.reservation.entity.model.User;
 import com.kaua.reservation.entity.repository.ResourceRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ResourceService {
+public class ResourceService extends AuthVerifyService {
 
     private final ResourceRepository resourceRepository;
 
@@ -18,6 +20,12 @@ public class ResourceService {
 
 
     public ResourceResponse createResource(ResourceRequest request){
+        User user = getAuthenticatedUser();
+
+
+
+
+
 
     }
 
