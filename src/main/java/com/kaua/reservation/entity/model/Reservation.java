b3d4646755bf37ba27b15305414a17d1770d036e
kaua <kaua.santanaj@gmail.com>
2026-01-14 14:49:42 -0300

    add status in model reservation

--- src/main/java/com/kaua/reservation/entity/model/Reservation.java
+++ src/main/java/com/kaua/reservation/entity/model/Reservation.java
@@ -1,6 +1,7 @@
 package com.kaua.reservation.entity.model;
 
 
+import com.kaua.reservation.entity.enums.ReservationStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,8 +23,11 @@ public class Reservation {
     @CreationTimestamp
     private LocalDateTime created_at;
 
-
     private LocalDateTime expires_at;
+
+    @Enumerated(EnumType.STRING)
+    private ReservationStatus status;
+
 
     @Version
     private Long version;
